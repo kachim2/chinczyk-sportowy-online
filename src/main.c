81186228c26040e168e121e1e8fdf38ae7fd40c1
kachim2 <michalrog14@gmail.com>
2023-07-19 13:53:49 +0200

    sbv

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 //  Program main entry point
 //------------------------------------------------------------------------------------
 const Color colors[] = {RED, BLUE, GREEN, YELLOW, VIOLET, BLACK};
+
 int32_t NumPlayers = 4;
 int32_t randi(int32_t min, int32_t max)
 {
@@ -23,26 +24,16 @@ typedef struct particle
     Vector2 v;
     Color color;
 } particle;
-Vector2 gravity(double m1, double m2, double rx, double ry, double t)
-{
-    double G = 0.01;
-    double soft = 0.001;
-    double r = sqrt((rx * rx) + (ry * ry));
-    double Fx = (G * m1 * m2 * rx) / (soft + r * r);
-    double Fy = (G * m1 * m2 * ry) / (soft + r * r);
-    double gx = Fx / m1;
-    double gy = Fy / m1;
-    double vx = gx * t;
-    double vy = gy * t;
-    Vector2 v;
-    v.x = vx;
-    v.y = vy;
-    return v;
-}
+
 void rolldice(int32_t *dice)
 {
     *dice = randi(1, 6);
 }
+typedef struct tile
+{
+    Color _color = BLACK;
+} tile;
+
 int main(void)
 {
 #ifdef __TINYC__
@@ -69,6 +60,7 @@ int main(void)
             positions[i][j] = -INT32_MAX;
         }
     }
+    tile tilemap[11][11];
     while (!WindowShouldClose()) // Detect window close button or ESC key
     {
         screenWidth = GetScreenWidth();
@@ -84,10 +76,7 @@ int main(void)
         ClearBackground(RAYWHITE);
         // DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
-        DrawRectangle(screenWidth / div, screenHeight / div, screenHeight / div, screenHeight / div, RED);
-        DrawRectangle(screenWidth / div, (screenHeight / div) * (div - 2), screenHeight / div, screenHeight / div, RED);
-        DrawRectangle((screenWidth / div) * (div - 2), screenHeight / div, screenHeight / div, screenHeight / div, RED);
-        DrawRectangle((screenWidth / div) * (div - 2), (screenHeight / div) * (div - 2), screenHeight / div, screenHeight / div, RED);
+
 
         EndDrawing();
         //----------------------------------------------------------------------------------
