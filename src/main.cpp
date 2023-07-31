920e88da305f5aa52563ec8bed6cb83c03d7b9bc
kachim2 <michalrog14@gmail.com>
2023-07-31 14:15:20 +0200

    added 16X msaa

--- src/main.cpp
+++ src/main.cpp
@@ -141,9 +141,9 @@ int main(void){
     //--------------------------------------------------------------------------------------
     int32_t screenWidth = 800;
     int32_t screenHeight = 800;
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT | FLAG_MSAA_4X_HINT);
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-
+    
     SetTargetFPS(60); // Set our game to run at refresh rate
     //--------------------------------------------------------------------------------------
     const float div = 7;
