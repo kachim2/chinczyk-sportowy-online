a9e5496ee7f8e5b9ae91ba5a3fc962c8219c8df3
kachim2 <michalrog14@gmail.com>
2023-09-29 09:03:42 +0200

    reliability improvements.

--- src/net.cpp
+++ src/net.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <memory>
 #include <netdb.h>
+#include <sys/ioctl.h>
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #include <emscripten/websocket.h>
@@ -34,13 +35,16 @@ void net_init(netdata* data){
 }
 
 bool net_ready(netdata* data){
-    std::cerr << recv(data->sock, NULL, 0, MSG_PEEK | MSG_TRUNC);
-    return recv(data->sock, NULL, 0, MSG_PEEK | MSG_TRUNC) >=0;
+    char tr[2];
+    std::cerr << recv(data->sock, tr, 2, MSG_PEEK | MSG_TRUNC);
+    return recv(data->sock, tr, 2, MSG_PEEK | MSG_TRUNC) >=2;
 }
 void net_rec(netdata* data){
     data->MovePlayerId = data->next;
     packeddata srvbuf;
+    //fcntl(data->sock, F_SETFL, fcntl(data->sock, F_GETFL, 0) | O_NONBLOCK;
     recv(data->sock, srvbuf.data, 2, 0);
+    //fcntl(data->sock, F_SETFL, fcntl(data->sock, F_GETFL, 0) | O_NONBLOCK);
     srvpack spacket = unpacksrv(srvbuf);
     data->MovePlayerId = data->next;
     data->MovePawnId = spacket.CurrPawnNum;
